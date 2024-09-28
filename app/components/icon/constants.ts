13959121d14dc7d6659ee4fa784ed6b1e7eb7e9c
leoromero97 <leonardo.g.romero.dev@gmail.com>
2024-09-28 17:28:49 -0300

    Add Footer - Remove favicon.icon unnecesary - Upgrade page.tsx file - Add asset pdf for CV - Upgrade constants and types in Icon for new assets - Create Footer component with our types, styles and logic - Create socialMedia.ts constant - Hide NavBar menu burger unnecesary

--- app/components/icon/constants.ts
+++ app/components/icon/constants.ts
@@ -1,8 +1,14 @@
+import Behance from "./components/Behance";
+import Calendar from "./components/Calendar";
 import Close from "./components/Close";
 import Download from "./components/Download";
+import Email from "./components/Email";
+import GitHub from "./components/Github";
 import IconRight from "./components/IconRight";
+import Linkedin from "./components/Linkedin";
 import LeoGerbacioRomeroLogo from "./components/LogoLeoGRomero";
 import WhatsApp from "./components/WhatsApp";
+import IconLocation from "./components/Location";
 
 export const icons = {
   Close,
@@ -10,4 +16,10 @@ export const icons = {
   IconRight,
   LeoGerbacioRomeroLogo,
   WhatsApp,
+  Behance,
+  Calendar,
+  Email,
+  GitHub,
+  Linkedin,
+  IconLocation,
 };
